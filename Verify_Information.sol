7a20c7d3d26af7d1f7fc3b3d57d59a29662d1052
anzinius <anzinius@gmail.com>
2020-10-07 06:16:00 +0900

    First commit

---
+++ Verify_Information.sol
@@ -0,0 +1,15 @@
+pragma ^0.4.19;
+
+contract Read{
+
+    struct Person {
+        string name;
+        uint poli;
+    }
+
+    Person[] public people;        //public, private
+
+    function _read(uint _numInfo, uint _id, uint _poli) private returns (uint){
+        return _poli
+    }
+}
