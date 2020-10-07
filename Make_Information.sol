9b600d650c209e8766ae7829a3f60c824ef9a0ca
anzinius <anzinius@gmail.com>
2020-10-07 19:32:38 +0900

    Make functions work

--- Make_Information.sol
+++ Make_Information.sol
@@ -1,30 +1,32 @@
-pragma ^0.4.19;
+pragma solidity >=0.4.24;
 
 contract MakeInformation{
     
+    uint256 keyIndex;
 
     struct Datum {
         string info;
         uint politId;
-        uint dna;
     }
 
-    Datum[] public data;        //public, private
-
-    function _createData(string _info, uint _politId, uint _dna) private {
-        data.push(Datum(_info, _politId, _dna));
+    mapping (uint256 => Datum) Data;
+    
+    function createData(string memory _info, uint _politId) public returns (uint256) {
+        Data[keyIndex].info = _info;
+        Data[keyIndex].politId = _politId;
+        keyIndex++;
+        return keyIndex;
     }
 
-    function _read(uint _numInfo, uint _id, uint _poli) private returns (uint){
-        return _poli;
+    function getInfo(uint _key) public view returns (string memory){
+        return Data[_key].info;
     }
 
     // function checkBalance(uint _numInfo, uint _id) returns (uint){
 
     // }
 
-    function createNewData(string _str) public returns (uint){
-        uint randDna = uint(keccak256(_str));
-        return randDna % (10 ** 16);
+    function getPolitId(uint _key) public view returns (uint){
+        return Data[_key].politId;
     }
 }
